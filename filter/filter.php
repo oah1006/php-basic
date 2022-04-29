d14aec0c586fbde9a80d916462a56eb9ed26fabb
oah1006 <bnhao10062001@gmail.com>
2022-04-29 16:37:08 +0700

    study filter basic

---
+++ filter/filter.php
@@ -0,0 +1,39 @@
+<?php
+
+// $string = "<h1>Hello this is Deverhub</h1>";
+// $newString = filter_var($string, FILTER_SANITIZE_STRING); // remove all html tags from string
+
+// $int = 50;
+
+// if (filter_var($int, FILTER_VALIDATE_INT)) { // CHECK INTEGER
+//     echo "this is integer";
+// } else {
+//     echo "this isn't integer";
+// }
+
+// $ip = "127.0.0.1";
+
+// if (!filter_var($ip, FILTER_VALIDATE_IP)) { // CHECK IP
+//     echo "$ip is not a valid";
+// } else {
+//     echo "$ip is valid";
+// }
+
+
+// $email = "nhathao10062001@gmail.com";
+
+// if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // check email
+//     echo "$email is valid"; 
+// } else {
+//     echo "$email is invalid";
+// }
+
+// $url = "https://www.google.com";
+
+// if (filter_var($url, FILTER_VALIDATE_URL)) { // check url
+//     echo "$url is valid";
+// } else {
+//     echo "$url is invalid";
+// }
+
+?>
