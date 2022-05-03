58c9d6111a207e8f1f7bbf2c3429c625d7376139
oah1006 <bnhao10062001@gmail.com>
2022-05-03 16:20:11 +0700

    update and delete mysqli

--- mysqli/index.php
+++ mysqli/index.php
@@ -2,22 +2,9 @@
 
 require 'connect.php';
 
-$result = $db->query("SELECT * FROM people");
-
-if($result) {
-    if($result->num_rows) {
-        
-        var_dump($result->num_rows);
-        
-        $row = $result->fetch_assoc();
-
-        while($row) {
-            echo $row["first_name"], " ", $row["last_name"], "<br>";
-        }
-        
-        echo "<pre>", var_dump($rows), "</pre>";
-    }
-}
+if ($update = $db->query("DELETE FROM people WHERE id = 1")) {
+    echo $db->affected_rows;
+}   
 
 
 ?>
